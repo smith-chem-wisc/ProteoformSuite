aee64a317aa856f8a68764bb59153b86f3eb1064
Anthony <anthony.cesnik@gmail.com>
2016-01-22 13:46:30 -0600

    Revert "Revert "Initial commit of proteoform suite framework""
    This reverts commit a9bf358646659b7e34a04b31bcc587af7480c3ba.

---
+++ ProteoformSuite/ProteoformSuite/ProteoformMatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteoformSuite
+{
+    class ProteoformMatcher
+    {
+        // TODO: implement ProteoformMatcher constructor
+        public ProteoformMatcher()
+        {
+
+        }
+
+        // TODO: Implement proteoform match
+        public MatchProteoforms()
+        {
+
+        }
+
+        // TODO: implement logic for GUI to view ET histogram from histogram?
+        // TODO: implement logic for GUI to view EE histogram from histogram?
+    }
+}
