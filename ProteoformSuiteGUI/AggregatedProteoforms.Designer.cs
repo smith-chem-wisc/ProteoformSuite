be0a2d26cacc7ff3e4f8ce1e5c851fde2cbd9cef
Leah Schaffer <lschaffer2@wisc.edu>
2016-07-19 09:24:18 -0500

    fixed ET Run the Gamut and small EE/ET relation issues

--- ProteoformSuiteGUI/AggregatedProteoforms.Designer.cs
+++ ProteoformSuiteGUI/AggregatedProteoforms.Designer.cs
@@ -205,7 +205,6 @@
             this.dgv_AcceptNeuCdLtProteoforms.RowTemplate.Height = 28;
             this.dgv_AcceptNeuCdLtProteoforms.Size = new System.Drawing.Size(988, 224);
             this.dgv_AcceptNeuCdLtProteoforms.TabIndex = 0;
-            this.dgv_AcceptNeuCdLtProteoforms.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_AcceptNeuCdLtProteoforms_CellContentClick);
             // 
             // AggregatedProteoforms
             // 
