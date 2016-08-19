624e8f5a63b1357cdd99aaa508725c56a2bb5c33
trishorts <mshort@chem.wisc.edu>
2016-08-19 13:42:39 -0500

    load quantitative results and added quantification form (#209)
    * load quantitative results and added quantification form
    
    * load quantitative results and added quantification form

---
+++ ProteoformSuiteGUI/Quantification.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ProteoformSuite
+{
+    partial class Quantification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Quantification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 728);
+            this.ControlBox = false;
+            this.Name = "Quantification";
+            this.Text = "Quantification";
+            this.Load += new System.EventHandler(this.Quantification_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
