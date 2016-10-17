e93ec0803beddb1de766bcd309f9bc95a91e6350
U-LAPTOP-8LS393FL\LeahSchaffer <lschaffer2@wisc.edu>
2016-10-17 18:05:55 -0500

    rerun

--- ProteoformSuiteInternal/ProteoformCommunity.cs
+++ ProteoformSuiteInternal/ProteoformCommunity.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ProteoformSuiteInternal;
 using System.Threading.Tasks;
-
+ 
 namespace ProteoformSuiteInternal
 {
     public class ProteoformCommunity
