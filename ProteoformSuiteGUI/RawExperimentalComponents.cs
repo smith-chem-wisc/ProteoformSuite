15b11906fcaa5f0eca4b65f67d5b0be4039fb6ff
Michael Shortreed <mshort@chem.wisc.edu>
2016-10-28 14:05:56 -0500

    removed missed monoisotopics and harmonics

--- ProteoformSuiteGUI/RawExperimentalComponents.cs
+++ ProteoformSuiteGUI/RawExperimentalComponents.cs
@@ -131,7 +131,7 @@ namespace ProteoformSuite
             dgv_RawQuantComp_MI_masses.AllowUserToAddRows = false;
             dgv_RawQuantComp_MI_masses.Columns["corrected_mass"].Visible = false;
             dgv_RawQuantComp_MI_masses.Columns["intensity_sum_olcs"].Visible = false;
-            dgv_RawQuantComp_MI_masses.Columns["_manual_mass_shift"].Visible = false;
+            //dgv_RawQuantComp_MI_masses.Columns["_manual_mass_shift"].Visible = false;
         }
 
         private void dgv_RawExpComp_MI_masses_CellContentClick(object sender, DataGridViewCellEventArgs e)
