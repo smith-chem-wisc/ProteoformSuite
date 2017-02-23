6a946e74923c9e28046c71a51ea4111b54583185
Anthony <anthony.cesnik@gmail.com>
2017-02-23 01:53:34 -0600

    TheoreticalDatabase speedup by parallelizing, simplifying finding unique positional PTM combinations, and using dictionaries to group by identical sequence and mass

--- ProteoformSuiteInternal/Modification.cs
+++ ProteoformSuiteInternal/Modification.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Proteomics;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace ProteoformSuiteInternal
 {
@@ -58,18 +60,18 @@ namespace ProteoformSuiteInternal
     public class PtmSet
     {
         public double mass;
-        private IEnumerable<Ptm> _ptm_combination;
-        public IEnumerable<Ptm> ptm_combination
+        private List<Ptm> _ptm_combination;
+        public List<Ptm> ptm_combination
         {
             get { return this._ptm_combination; }
             set
             {
                 this._ptm_combination = value;
-                this.mass = value.Select(ptm => ptm.modification.monoisotopicMass).Sum();
+                this.mass = value.Sum(ptm => ptm.modification.monoisotopicMass);
             }
         }
 
-        public PtmSet(IEnumerable<Ptm> unique_ptm_combination)
+        public PtmSet(List<Ptm> unique_ptm_combination)
         {
             this.ptm_combination = unique_ptm_combination;
         }
@@ -80,11 +82,11 @@ namespace ProteoformSuiteInternal
         public List<Ptm> all_ptms;
         public PtmCombos(IDictionary<int, List<Modification>> ptm_data)
         {
-            this.all_ptms = new List<Ptm>(
+            this.all_ptms = (
                 from position in ptm_data.Keys
                 from modification in ptm_data[position].OfType<ModificationWithMass>()
-                select new Ptm(position, modification)
-            );
+                select new Ptm(position, modification))
+            .OrderBy(p => p.position).ToList();
         }
 
         //Gets unique-mass (given a tolerance) ptm combinations from all the possible ones without positional redundancy
@@ -92,38 +94,54 @@ namespace ProteoformSuiteInternal
         //Includes an empty combination with no PTMS
         public List<PtmSet> get_combinations(int num_ptms_needed)
         {
-            List<PtmSet> combinations = all_possible_combinations(num_ptms_needed);
-            List<PtmSet> unique_positional_combinations = combinations.Where(set => set.ptm_combination.Select(ptm => ptm.position).Count() //where the length of the positions list
-                == new HashSet<int>(set.ptm_combination.Select(ptm => ptm.position)).Count).ToList(); //is the same as the number of unique positions
+            List<PtmSet> combinations = all_unique_positional_combinations(num_ptms_needed);
             List<PtmSet> unique_mass_combinations = new List<PtmSet>();
-            foreach (PtmSet combination in unique_positional_combinations)
+
+            //I tried speeding this up by removing all similar masses instead; it doesn't speed it up... AC170223
+            foreach (PtmSet combination in combinations)
             {
                 double lower_mass = combination.mass - Lollipop.ptmset_mass_tolerance;
                 double upper_mass = combination.mass + Lollipop.ptmset_mass_tolerance;
-                if (unique_mass_combinations.Where(c => c.mass >= lower_mass && c.mass <= upper_mass).Count() == 0)
+                if (unique_mass_combinations.All(c => !(c.mass >= lower_mass && c.mass <= upper_mass)))
                     unique_mass_combinations.Add(combination);
             }
+
             unique_mass_combinations.Add(new PtmSet(new List<Ptm>()));
             return unique_mass_combinations;
         }
 
         //For each length up to the maximum number of ptms (or the max number of modifications in this list),
         //generate all the combinations, with the shortest combinations first, and with the modifications at the first positions first
-        private List<PtmSet> all_possible_combinations(int max_length)
+        private List<PtmSet> all_unique_positional_combinations(int max_length)
         {
             max_length = Math.Min(max_length, this.all_ptms.Count);
             List<PtmSet> combos = new List<PtmSet>(
                 from i in Enumerable.Range(1, max_length)
-                from combination in combinations(i)
+                from combination in unique_positional_combinations(i)
+                //where combination.ptm_combination.Count == combination.ptm_combination.DistinctBy(c => c.position).Count() //where the length of the positions list is the same as the number of unique positions
                 select combination
             );
             return combos;
         }
 
-        //Generates all the combinations of a certain length
-        private IEnumerable<PtmSet> combinations(int combination_length)
+        //For each length up to the maximum number of ptms (or the max number of modifications in this list),
+        //generate all the combinations, with the shortest combinations first, and with the modifications at the first positions first
+        //private List<PtmSet> all_possible_combinations(int max_length)
+        //{
+        //    max_length = Math.Min(max_length, this.all_ptms.Count);
+        //    List<PtmSet> combos = new List<PtmSet>(
+        //        from i in Enumerable.Range(1, max_length)
+        //        from combination in combinations(i)
+        //        select combination
+        //    );
+        //    return combos;
+        //}
+
+        //Generates all combinations of a certain length with unique positions
+        private IEnumerable<PtmSet> unique_positional_combinations(int combination_length)
         {
             Ptm[] result = new Ptm[combination_length];
+            int prev_position = -2;
             Stack<int> stack = new Stack<int>();
             stack.Push(0);
 
@@ -134,20 +152,54 @@ namespace ProteoformSuiteInternal
                 Ptm value = this.all_ptms[mod_index];
                 while (mod_index < this.all_ptms.Count)
                 {
-                    result[result_index] = this.all_ptms[mod_index];
-                    result_index++;
+                    value = this.all_ptms[mod_index];
+                    result[result_index] = value;
                     mod_index++;
+                    if (prev_position == value.position) continue;
+                    prev_position = value.position;
+                    result_index++;
                     if (mod_index < this.all_ptms.Count)
                         stack.Push(mod_index);
                     if (result_index == combination_length)
                     {
                         Ptm[] destinationArray = new Ptm[combination_length];
                         Array.Copy(result, destinationArray, combination_length);
-                        yield return new PtmSet(destinationArray);
+                        yield return new PtmSet(destinationArray.ToList());
+                        prev_position = -2;
                         break;
                     }
                 }
             }
         }
+
+        ////Generates all the combinations of a certain length
+        //private IEnumerable<PtmSet> combinations(int combination_length)
+        //{
+        //    Ptm[] result = new Ptm[combination_length];
+        //    Stack<int> stack = new Stack<int>();
+        //    stack.Push(0);
+
+        //    while (stack.Count > 0)
+        //    {
+        //        int result_index = stack.Count - 1;
+        //        int mod_index = stack.Pop();
+        //        Ptm value = this.all_ptms[mod_index];
+        //        while (mod_index < this.all_ptms.Count)
+        //        {
+        //            result[result_index] = this.all_ptms[mod_index];
+        //            result_index++;
+        //            mod_index++;
+        //            if (mod_index < this.all_ptms.Count)
+        //                stack.Push(mod_index);
+        //            if (result_index == combination_length)
+        //            {
+        //                Ptm[] destinationArray = new Ptm[combination_length];
+        //                Array.Copy(result, destinationArray, combination_length);
+        //                yield return new PtmSet(destinationArray.ToList());
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
--- ProteoformSuiteInternal/Proteoform.cs
+++ ProteoformSuiteInternal/Proteoform.cs
@@ -661,16 +661,29 @@ namespace ProteoformSuiteInternal
     {
         public List<string> accessionList { get; set; } // this is the list of accession numbers for all proteoforms that share the same modified mass. the list gets alphabetical order
 
-        public TheoreticalProteoformGroup(List<TheoreticalProteoform> theoreticals)
+        public TheoreticalProteoformGroup(List<TheoreticalProteoform> theoreticals, bool contaminants, Dictionary<InputFile, Protein[]> theoretical_proteins)
             : base(theoreticals[0].accession + "_T" + theoreticals.Count(), String.Join(";", theoreticals.Select(t => t.description)), String.Join(";", theoreticals.Select(t => t.description)), String.Join(";", theoreticals.Select(t => t.fragment)), theoreticals[0].begin, theoreticals[0].end, theoreticals[0].unmodified_mass, theoreticals[0].lysine_count, theoreticals[0].goTerms, theoreticals[0].ptm_set, theoreticals[0].modified_mass, theoreticals[0].is_target)
         {
             this.accessionList = theoreticals.Select(p => p.accession).ToList();
             this.proteinList = theoreticals.SelectMany(p => p.proteinList).ToList();
-        }
-
-        public override void check_contaminant_status(Dictionary<InputFile, Protein[]> theoretical_proteins)
-        {
-            this.contaminant = theoretical_proteins.Where(item => item.Key.ContaminantDB).SelectMany(kv => kv.Value).Any(p => this.accessionList.Select(acc => acc.Split(new char[] { '_' })[0]).Contains(p.Accession));
+            if (contaminants)
+            {
+                List<Protein> matching_contaminants = theoretical_proteins.Where(item => item.Key.ContaminantDB).SelectMany(kv => kv.Value).Where(p => this.accessionList.Select(acc => acc.Split(new char[] { '_' })[0]).Contains(p.Accession)).ToList();
+                this.contaminant = matching_contaminants.Count > 0;
+                this.accession = matching_contaminants[0].Accession + "_T" + accessionList.Count();
+                this.description = String.Join(";", matching_contaminants.Select(t => t.FullDescription));
+                this.name = String.Join(";", matching_contaminants.Select(t => t.FullDescription));
+                TheoreticalProteoform first_contaminant = theoreticals.FirstOrDefault(t => t.accession.Contains(matching_contaminants[0].Accession));
+                this.begin = first_contaminant.begin;
+                this.end = first_contaminant.end;
+                this.unmodified_mass = first_contaminant.unmodified_mass;
+                this.modified_mass = first_contaminant.modified_mass;
+                this.lysine_count = first_contaminant.lysine_count;
+                this.goTerms = first_contaminant.goTerms;
+                this.ptm_set = first_contaminant.ptm_set;
+                this.is_target = first_contaminant.is_target;
+                this.is_decoy = first_contaminant.is_decoy;
+            }
         }
     }
 
