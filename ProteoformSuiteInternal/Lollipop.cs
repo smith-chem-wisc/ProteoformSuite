cfa5ef0107a1447952ff1ee58ee18617a26fd9fa
Anthony <anthony.cesnik@gmail.com>
2017-04-13 16:27:16 -0500

    Clear variable oxidations if remaking the database

--- ProteoformSuiteInternal/Lollipop.cs
+++ ProteoformSuiteInternal/Lollipop.cs
@@ -481,8 +481,9 @@ namespace ProteoformSuiteInternal
 
             foreach (string filename in Directory.GetFiles(Path.Combine(current_directory, "Mods")))
             {
-                if (filename.EndsWith("variable.txt") && !methionine_oxidation) continue;
-                IEnumerable<ModificationWithLocation> new_mods = PtmListLoader.ReadModsFromFile(filename);
+                IEnumerable<ModificationWithLocation> new_mods = !filename.EndsWith("variable.txt") || methionine_oxidation ?
+                    PtmListLoader.ReadModsFromFile(filename) :
+                    new List<ModificationWithLocation>(); // Empty variable modifications if not selected
                 if (filename.EndsWith("variable.txt")) variableModifications = new_mods.OfType<ModificationWithMass>().ToList();
                 all_known_modifications.AddRange(new_mods);
             }
