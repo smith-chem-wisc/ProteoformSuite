2dd19521592f579e3feeecbc57c947093c2d3b99
Anthony <anthony.cesnik@gmail.com>
2017-05-20 18:20:16 -0500

    Unlocalized Modification and custom mod names

---
+++ ProteoformSuiteInternal/UnlocalizedModification.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proteomics;
+
+namespace ProteoformSuiteInternal
+{
+    public class UnlocalizedModification
+    {
+
+        #region Public Properties
+
+        public double mass { get; set; }
+        public string id { get; set; }
+        public int ptm_count { get; set; }
+        public ModificationWithMass original_modification { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Constructor
+
+        public UnlocalizedModification(ModificationWithMass m)
+        {
+            original_modification = m;
+            mass = m.monoisotopicMass;
+            id = m.id;
+            ptm_count = 1;
+        }
+
+        #endregion Public Constructor
+    }
+}
