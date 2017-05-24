c70e26fa4694a0f1b4ffb9bfcab22517d2b987c4
Anthony <anthony.cesnik@gmail.com>
2017-05-23 23:42:44 -0500

    Allow loss of multiple PTMs. Allow requirement for proteoform without a specific mod (e.g. ammonia loss)

--- ProteoformSuiteInternal/UnlocalizedModification.cs
+++ ProteoformSuiteInternal/UnlocalizedModification.cs
@@ -10,6 +10,7 @@ namespace ProteoformSuiteInternal
         public double mass { get; set; }
         public string id { get; set; }
         public int ptm_count { get; set; }
+        public bool require_proteoform_without_mod { get; set; }
         public int ptm_rank { get; set; }
         public ModificationWithMass original_modification { get; set; }
 
@@ -24,6 +25,7 @@ namespace ProteoformSuiteInternal
             id = m.id;
             ptm_count = 1;
             ptm_rank = SaveState.lollipop.modification_ranks[m.monoisotopicMass];
+            require_proteoform_without_mod = false;
 
             if (m.modificationType == "FattyAcid" || m.modificationType == "Unlocalized")
                 ptm_rank = SaveState.lollipop.mod_rank_first_quartile / 2;
