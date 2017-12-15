73957eaeb34eacd5de1fc73475f422844f056544
Anthony <anthony.cesnik@gmail.com>
2017-12-15 01:10:30 -0600

    Allow and use relative paths for results folder and all input files.

--- Test/TestSaveState.cs
+++ Test/TestSaveState.cs
@@ -61,7 +61,7 @@ namespace Test
                 else continue;
             }
 
-            Sweet.open_method(builder.ToString(), false, out string warning);
+            Sweet.open_method("", builder.ToString(), false, out string warning);
             foreach (PropertyInfo property in typeof(Lollipop).GetProperties())
             {
                 if (property.PropertyType == typeof(int))
@@ -120,7 +120,7 @@ namespace Test
             Sweet.unaccept_peak_action(pr2);
             using (StreamWriter file = new StreamWriter(Path.Combine(TestContext.CurrentContext.TestDirectory, "method.xml")))
                 file.WriteLine(Sweet.save_method());
-            Sweet.open_method(String.Join(Environment.NewLine, File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "method.xml"))), true, out string warning);
+            Sweet.open_method(Path.Combine(TestContext.CurrentContext.TestDirectory, "method.xml"), String.Join(Environment.NewLine, File.ReadAllLines(Path.Combine(TestContext.CurrentContext.TestDirectory, "method.xml"))), true, out string warning);
             Sweet.lollipop.ee_peaks = test_community.accept_deltaMass_peaks(prs2, new List<ProteoformRelation>());
             Assert.AreEqual(1, Sweet.lollipop.ee_peaks.Count);
             DeltaMassPeak peak = Sweet.lollipop.ee_peaks[0];
