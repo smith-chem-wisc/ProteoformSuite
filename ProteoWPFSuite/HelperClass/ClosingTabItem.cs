e8e2f5942d76d6d0963aa46b3c9dc1feffbc3f97
xrsheeran <xqu25@wisc.edu>
2019-07-09 13:27:04 -0500

    Front-End Fixes

--- ProteoWPFSuite/HelperClass/ClosingTabItem.cs
+++ ProteoWPFSuite/HelperClass/ClosingTabItem.cs
@@ -20,12 +20,7 @@ namespace ProteoWPFSuite
         {
             freeze = false;
             header = new ClosingTabHeader();
-            header.tab_close.MouseEnter += new MouseEventHandler(button_close_MouseEnter);
-            header.tab_close.MouseLeave += new MouseEventHandler(button_close_MouseLeave);
-            header.tab_close.Click += new RoutedEventHandler(button_close_Click);
-            header.tab_title.SizeChanged += new SizeChangedEventHandler(label_TabTitle_SizeChanged);
             this.Header = header;
-            ((ClosingTabHeader)this.Header).tab_close.Visibility = Visibility.Hidden;
         }
         private ClosingTabHeader header;
         public string Title
@@ -37,12 +32,10 @@ namespace ProteoWPFSuite
         protected override void OnSelected(RoutedEventArgs e)
         {
             base.OnSelected(e);
-            ((ClosingTabHeader)this.Header).tab_close.Visibility = Visibility.Visible;
         }
         protected override void OnUnselected(RoutedEventArgs e)
         {
             base.OnUnselected(e);
-            ((ClosingTabHeader)this.Header).tab_close.Visibility = Visibility.Hidden;
         }
         protected override void OnMouseEnter(MouseEventArgs e)
         {
@@ -51,34 +44,14 @@ namespace ProteoWPFSuite
                 return;
             }
             base.OnMouseEnter(e);
-            ((ClosingTabHeader)this.Header).tab_close.Visibility = Visibility.Visible;
         }
         protected override void OnMouseLeave(MouseEventArgs e)
         {
             base.OnMouseLeave(e);
-            if (!this.IsSelected)
-            {
-                ((ClosingTabHeader)this.Header).tab_close.Visibility = Visibility.Hidden;
-            }
         }
         // Button MouseEnter - When the mouse is over the button - change color to Red
-        private void button_close_MouseEnter(object sender, MouseEventArgs e)
-        {
-            if (freeze)
-            {
-                return;
-            }
-            ((ClosingTabHeader)this.Header).tab_close.Foreground = Brushes.Red;
-        }
-        // Button MouseLeave - When mouse is no longer over button - change color back to black
-        private void button_close_MouseLeave(object sender, MouseEventArgs e)
-        {
-            if (freeze)
-            {
-                return;
-            }
-            ((ClosingTabHeader)this.Header).tab_close.Foreground = Brushes.Black;
-        }
+        
+        
         // Button Close Click - Remove the Tab - (or raise
         // an event indicating a "CloseTab" event has occurred)
         private void button_close_Click(object sender, RoutedEventArgs e)
@@ -89,16 +62,6 @@ namespace ProteoWPFSuite
             }
             ((TabControl)this.Parent).Items.Remove(this);
         }
-        // Label SizeChanged - When the Size of the Label changes
-        // (due to setting the Title) set position of button properly
-        private void label_TabTitle_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (freeze)
-            {
-                return;
-            }
-            ((ClosingTabHeader)this.Header).tab_close.Margin = new Thickness(
-            ((ClosingTabHeader)this.Header).tab_title.ActualWidth + 5, 3, 4, 0);
-        }
+       
     }
 }
