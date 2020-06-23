3a18096b011c1b381c9e2503d6df6bf8e9ad9754
Leah <lschaffer2@wisc.edu>
2020-06-23 15:07:55 -0500

    unit test fix

--- Test/TestFLASHDeconv.cs
+++ Test/TestFLASHDeconv.cs
@@ -18,19 +18,17 @@ namespace Test
 
             Sweet.lollipop.enter_input_files(new string[] { Path.Combine(TestContext.CurrentContext.TestDirectory, "05-26-17_B7A_yeast_td_fract5_rep1.mzML") }, Lollipop.acceptable_extensions[4], Lollipop.file_types[4], Sweet.lollipop.input_files, false);
 
-            string filepath = Path.Combine(TestContext.CurrentContext.TestDirectory, Path.GetFileNameWithoutExtension(Sweet.lollipop.input_files[0].complete_path));
-
             // Make sure no initial problems with running deconvolution.
             Assert.AreEqual("Successfully deconvoluted 1 raw file.", Sweet.lollipop.flash_deconv(60, 1, TestContext.CurrentContext.TestDirectory));
 
             // Ensure the deconvolution output a file.
-            Assert.IsTrue(File.Exists(filepath + "_decon.tsv"));
+            Assert.IsTrue(File.Exists(Path.Combine(TestContext.CurrentContext.TestDirectory, "05-26-17_B7A_yeast_td_fract5_rep1_decon.tsv")));
 
             //Check contents of file to ensure number of components match.
             Sweet.lollipop = new Lollipop();
             Sweet.lollipop.min_likelihood_ratio = -100000;
             Sweet.lollipop.max_fit = 10000;
-            Sweet.lollipop.enter_input_files(new string[] { filepath + "_decon.tsv" }, Lollipop.acceptable_extensions[0], Lollipop.file_types[0], Sweet.lollipop.input_files, false);
+            Sweet.lollipop.enter_input_files(new string[] { Path.Combine(TestContext.CurrentContext.TestDirectory, "05-26-17_B7A_yeast_td_fract5_rep1_decon.tsv" )}, Lollipop.acceptable_extensions[0], Lollipop.file_types[0], Sweet.lollipop.input_files, false);
             List<Component> deconv_components = new List<Component>();
             Sweet.lollipop.process_raw_components(Sweet.lollipop.input_files.Where(f => f.purpose == Purpose.Identification).ToList(), deconv_components, Purpose.Identification, false);
             Assert.AreEqual(54, deconv_components.Count);
